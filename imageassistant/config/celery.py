a9fff99d8910ad0c14a51e4261dc75b0350f77c0
Andreas Cormack <andreas@passthekeys.co.uk>
2024-08-19 21:17:41 +0100

    test ansible deployment

--- imageassistant/config/celery.py
+++ imageassistant/config/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.production_settings")
 
 
 app = Celery("config")
