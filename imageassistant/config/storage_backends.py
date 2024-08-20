775c98848ebb69f1bf133f4296ae3b8b7ba37b26
Andreas Cormack <andreas@passthekeys.co.uk>
2024-08-20 18:50:40 +0100

    fix post delete

---
+++ imageassistant/config/storage_backends.py
@@ -0,0 +1,12 @@
+# myapp/storage_backends.py
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'public-read'
+
+
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = True
