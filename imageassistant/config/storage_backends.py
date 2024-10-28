08f2a5b0c59be076254fad17e332372f2cbead8e
Andreas Cormack <andreas@passthekeys.co.uk>
2024-10-28 20:11:40 +0000

    adding whitenoise to project

--- imageassistant/config/storage_backends.py
+++ imageassistant/config/storage_backends.py
@@ -1,9 +1,11 @@
 # myapp/storage_backends.py
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.contrib.staticfiles import storage
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(storage.StaticFilesStorage):
     location = 'static'
+    file_overwrite = True
 
 
 class MediaStorage(S3Boto3Storage):
