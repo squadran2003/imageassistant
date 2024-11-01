9d9c3ef5307e0595b589cc6ae3629406736b4b0c
Andreas Cormack <andreas@passthekeys.co.uk>
2024-11-01 12:13:14 +0000

    added cloudfront

--- imageassistant/config/production_settings.py
+++ imageassistant/config/production_settings.py
@@ -28,16 +28,16 @@ INSTALLED_APPS = [
     'api',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     "whitenoise.middleware.WhiteNoiseMiddleware",
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
 
 
 DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
--- imageassistant/config/storage_backends.py
+++ imageassistant/config/storage_backends.py
@@ -2,6 +2,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
+
 class StaticStorage(S3Boto3Storage):
     location = 'static'
     file_overwrite = True
