ea994f4f5bb9538ef3ec53103a73587fee50e5f0
Andreas Cormack <andreas@passthekeys.co.uk>
2024-12-17 13:54:43 +0000

    fixing bug with indicator on home page

--- imageassistant/components/pages/main_content.py
+++ imageassistant/components/pages/main_content.py
@@ -11,11 +11,14 @@ class MainContent(Component):
                             <div class="col s12 m12 l12 x12">
                                 <div class="banner-text">
                                     <h1 class="banner">Unlock Your Image's Full Potential</h1>
-                                    <h5>Transform, Enhance, and Personalize your images with ImageAssistant's powerful tools.
+                                    <h5>Transform and Enhance your images with ImageAssistant's powerful tools.
                                         Start creating stunning visuals in just a few clicks!
                                     </h5>
                                 </div>
                                 {% component "upload_component" id="upload-form" target="#content" swap="innerHTML"  url="images:add" form="{{form}}" %}{% endcomponent %}
+                                    <div class="htmx-indicator progress" id="indicator">
+                                        <div class="indeterminate"></div>
+                                    </div>
                             </div>
                         </div>
                     </div>
