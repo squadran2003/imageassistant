fa9415a11578896ef07ed02e8f44a4e24d9a3966
Andreas Cormack <andreas@passthekeys.co.uk>
2025-01-31 12:31:51 +0000

    re styled with tailwind and changed services

--- imageassistant/config/production_settings.py
+++ imageassistant/config/production_settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "template_partials",
     'django_components',
     'rest_framework',
     'storages',
