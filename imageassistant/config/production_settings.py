bec43725fdb9f94a93971b88444c318ec18f3a4e
Andreas Cormack <andreas@passthekeys.co.uk>
2025-04-17 13:36:20 +0100

    restyled the search and nav bar

--- imageassistant/config/production_settings.py
+++ imageassistant/config/production_settings.py
@@ -82,7 +82,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 }
 AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3.S3Storage'
 DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
 CLOUDFRONT_DOMAIN = SECRETS['CLOUDFRONT_DOMAIN']
 STATIC_HOST = CLOUDFRONT_DOMAIN
