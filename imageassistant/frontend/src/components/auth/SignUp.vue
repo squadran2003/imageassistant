30c7c7e65be5238c59223c2dbc7bd916412de627
Andreas Cormack <cormackandy@hotmail.com>
2025-08-10 08:10:26 +0100

    Update SignUp.vue

--- imageassistant/frontend/src/components/auth/SignUp.vue
+++ imageassistant/frontend/src/components/auth/SignUp.vue
@@ -364,6 +364,7 @@ export default {
     async handleGoogleCredentialResponse(response) {
         // Send the ID token to your backend
         try {
+          console.log(`${this.apiUrl}/api/v1/google/login/`);
           const backendResponse = await fetch(`${this.apiUrl}/api/v1/google/login/`, {
             method: 'POST',
             headers: {
